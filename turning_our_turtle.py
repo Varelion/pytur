b4efbe65ccd11f89fa35339d6a47020a541bb78d
Varelion <business.8508@gmail.com>
2023-11-30 13:07:52 -0500

    Change turning_our_turtle.py to 50 units

---
+++ turning_our_turtle.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+# Moves forward
+forward(100)
+# Turns 90°
+right(90)
+# Repeat until a square is complete
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(50)
+done()
