06016133ac2b92e9d3acbc92f805101911f03f8d
Varelion <business.8508@gmail.com>
2023-11-30 13:55:32 -0500

    Establish filling_in_shapes.py

---
+++ filling_in_shapes.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+begin_fill()
+circle(50)
+end_fill()
+
+forward(300)
+
+color("green")
+
+begin_fill()
+circle(30)
+end_fill()
+
+right(90)
+forward(100)
+
+begin_fill()
+color("purple")
+circle(10)
+end_fill()
+
+done()
