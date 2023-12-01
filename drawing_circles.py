9cccd07534012330dd25446c882077b4f9cf644f
Varelion <business.8508@gmail.com>
2023-12-01 08:14:31 -0500

    Modify drawing_circles.py

--- drawing_circles.py
+++ drawing_circles.py
@@ -1,9 +1,17 @@
 from turtle import *
-
+speed(0)
 # Here we create a circle with a radius, not diameter, or 50 units.
 circle (50)
+penup()
 forward(100)
+pendown()
 circle(50)
 # Given radius is half a circle, by moving forward 100 units, the second radius circle is touching the first without overlap.
+penup()
+backward(100)
+right(90)
+forward(35)
+pendown()
+circle(50)
 
 done()
