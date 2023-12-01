1ed8191d5f067700f96a34080c15ed24e14741fb
Varelion <business.8508@gmail.com>
2023-12-01 09:11:23 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+a = 5
+b = 8
+
+if (a == b):
+	print("A is equal to b.")
+	a = 10
+	print(a)
+
+if (a > b):
+	print ("A is greater than b.")
+
+if (a < b):
+	print("A is less tan b.")
+
+def say_hello(name):
+	print("Hello " + name)
+
+print (say_hello("Alex"))
