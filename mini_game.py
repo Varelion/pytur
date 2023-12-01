834fe489901399418cf264a836f8d85981378cef
Varelion <business.8508@gmail.com>
2023-12-01 12:17:21 -0500

    Finish mini_game.py project

--- mini_game.py
+++ mini_game.py
@@ -23,31 +23,78 @@ from turtle import *
 
 bgcolor("#D2691E")
 speed(0)
-move_distance = 20
+move_distance = 50
 wide = window_width()/2
 tall = window_height()/2
+ocean_start = wide-wide/3
+ocean_end = round(wide)
+goal = ocean_start
 
 def game_init():
 	# Draws the ocean on the sand
 	penup()
 	color("blue")
 	begin_fill()
-	goto(round(wide-wide/3), tall)
+	goto(ocean_start, tall)
 	pendown()
-	goto(round(wide), tall)
-	goto(round(wide), -tall)
-	goto(round(wide-wide/3), -tall)
-	goto(round(wide-wide/3), tall)
+	goto(ocean_end, tall)
+	goto(ocean_end, -tall)
+	goto(ocean_start, -tall)
+	goto(ocean_start, tall)
 	end_fill()
 
-def move_turtle():
+def move_to_starting_position():
 	penup()
 	goto(-wide+10,0)
 	pendown()
 	dot(15,"white")
 	color("green")
 	write("The egg has hatched!")
+	shape("turtle")
+
+def check_goal():
+	turtle_position = xcor()
+	global goal
+	if goal < turtle_position:
+		hideturtle()
+		penup()
+		goto(0,0)
+		color("white")
+		write("YOU WIN!")
+		onkey(None, "Up")
+		onkey(None, "Down")
+		onkey(None, "Left")
+		onkey(None, "Right")
+
 
 game_init()
-move_turtle()
+move_to_starting_position()
+
+def move_up():
+	setheading(90)
+	forward(move_distance)
+	check_goal()
+
+def move_down():
+	setheading(270)
+	forward(move_distance)
+	check_goal()
+
+def move_left():
+	setheading(180)
+	forward(move_distance)
+	check_goal()
+
+def move_right():
+	setheading(0)
+	forward(move_distance)
+	check_goal()
+
+onkey(move_up, "Up")
+onkey(move_down, "Down")
+onkey(move_left, "Left")
+onkey(move_right, "Right")
+
+
+listen()
 done()
